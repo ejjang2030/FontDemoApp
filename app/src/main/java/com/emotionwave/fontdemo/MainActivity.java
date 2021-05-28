545373a0d005489098fa477f29c1100c8e81a193
ejjang2030 <ejjang2030@gmail.com>
2021-05-28 18:50:07 +0900

    add shader function

--- app/src/main/java/com/emotionwave/fontdemo/MainActivity.java
+++ app/src/main/java/com/emotionwave/fontdemo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -26,14 +27,14 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.emotionwave.fontdemo.customview.StrokeTextView;
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     boolean isBold = false;
     boolean isItalic = false;
     boolean isBackground = false;
+    boolean isColorPanel = false;
+    boolean isShader = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView.setTextColor(Color.RED);
+                textView.invalidate();
             }
         });
         Button green = findViewById(R.id.green);
@@ -102,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView.setTextColor(Color.GREEN);
+                textView.invalidate();
             }
         });
         Button blue = findViewById(R.id.blue);
@@ -109,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView.setTextColor(Color.BLUE);
+                textView.invalidate();
             }
         });
         Button strokered = findViewById(R.id.stroke_red);
@@ -117,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setStrokeWidth(3);
                 textView.setStrokeColor(Color.RED);
+                textView.invalidate();
                 //textView.setTypeface(textView.getTypeface(), textView.getTypeface().getStyle());
             }
         });
@@ -126,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setStrokeWidth(3);
                 textView.setStrokeColor(Color.GREEN);
+                textView.invalidate();
                 //textView.setTypeface(textView.getTypeface(), textView.getTypeface().getStyle());
             }
         });
@@ -135,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textView.setStrokeWidth(3);
                 textView.setStrokeColor(Color.BLUE);
+                textView.invalidate();
                 //textView.setTypeface(textView.getTypeface(), textView.getTypeface().getStyle());
             }
         });
@@ -143,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isBold) {
-                    textView.setTypeface(textView.getTypeface(), Typeface.NORMAL);
+                    textView.setTypeface(null, Typeface.NORMAL);
                     isBold = false;
                 } else {
                     textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
@@ -156,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isItalic) {
-                    textView.setTypeface(textView.getTypeface(), Typeface.NORMAL);
+                    textView.setTypeface(null, Typeface.NORMAL);
                     isItalic = false;
                 } else {
                     textView.setTypeface(textView.getTypeface(), Typeface.ITALIC);
@@ -192,14 +201,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // https://www.youtube.com/watch?v=q2GtM1_RmMw 참고
                 if(isBackground) {
-                    textView.getPaint().setShader(null);
+                    Shader shader = new LinearGradient(0, 0, 0, textView.getTextSize(), Color.BLACK, Color.BLACK, Shader.TileMode.CLAMP);
+                    textView.getPaint().setShader(shader);
+                    textView.invalidate();
                     isBackground = false;
                 } else {
                     Shader shader = new LinearGradient(0, 0, 0, textView.getTextSize(), Color.RED, Color.BLUE, Shader.TileMode.CLAMP);
                     textView.getPaint().setShader(shader);
+                    textView.invalidate();
                     isBackground = true;
                 }
 
+            }
+        });
+        Button shader = findViewById(R.id.shader);
+        shader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isShader) {
+                    isShader = false;
+                    textView.setShadowLayer(0.0f, 0.0f, 0.0f, Color.BLACK);
+                } else {
+                    isShader = true;
+                    textView.setShadowLayer(9.0f, 3.0f, 3.0f, Color.BLUE);
+                }
             }
         });
     }
